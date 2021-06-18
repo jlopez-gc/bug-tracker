46948ffb588461e46412d4196e8fc980dfb5d6d4
José María López Díaz <jlopez.diaz.gcc@gmail.com>
2021-06-18 14:14:55 +0100

    Modify controller mapping and add Response Status to methods

--- api/src/main/java/com/jlopez/bugtracker/controller/BugController.java
+++ api/src/main/java/com/jlopez/bugtracker/controller/BugController.java
@@ -3,26 +3,26 @@ package com.jlopez.bugtracker.controller;
 import com.jlopez.bugtracker.model.BugPayload;
 import com.jlopez.bugtracker.service.BugService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/bugs")
+@RequestMapping("/bug")
 @AllArgsConstructor
 public class BugController {
 
     private final BugService bugService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<BugPayload> findAll() {
         return bugService.findAll();
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public BugPayload findById(@PathVariable Long id) {
         return bugService.findById(id);
     }
